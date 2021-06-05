b7a427a8058021b7ad1e5ccd2225776eb57a2256
Chris <christopher-dags@hotmail.com>
2021-06-05 13:56:35 -0400

    created service methods for retrieving states and countries

--- src/app/services/luv2-shop-form.service.ts
+++ src/app/services/luv2-shop-form.service.ts
@@ -2,6 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Country } from '../common/country';
+import { State } from '../common/state';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +15,21 @@ export class Luv2ShopFormService {
   private statesUrl = 'http://localhost:8080/api/states';
 
   constructor(private httpClient: HttpClient) { }
+
+  getCountries(): Observable<Country[]>{
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+      map(response => response._embedded.countries)
+    );
+  }
+
+  getStates(countryCode: string): Observable<State[]>{
+    
+    const searchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${countryCode}`;
+    
+    return this.httpClient.get<GetResponseStates>(searchStatesUrl).pipe(
+      map(response => response._embedded.states)
+    );
+  }
 
   getCreditCardMonths(startMonth: number): Observable<number[]>{
     let data: number[] = [];
@@ -35,5 +53,17 @@ export class Luv2ShopFormService {
     // 'of' operator wraps the object as an observable
     return of(data);
 
+  }
+}
+
+interface GetResponseCountries{
+  _embedded:{
+    countries: Country[];
+  }
+}
+
+interface GetResponseStates{
+  _embedded:{
+    states: State[];
   }
 }
