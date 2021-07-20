9373ffb25bc3994cffbf7dfa6df83e999905b74d
Chris <christopher-dags@hotmail.com>
2021-07-20 17:12:18 -0400

    Changed totalQuantity and totalPrice to BehaviorSubject and subscribed to them in checkout.component

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
+import { CartService } from 'src/app/services/cart.service';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
 import { Luv2ShopValidators } from 'src/app/validators/luv2-shop-validators';
 
@@ -25,9 +26,13 @@ export class CheckoutComponent implements OnInit {
   shippingAddressStates: State[] = [];
   billingAddressStates: State[] = [];
 
-  constructor(private formBuilder: FormBuilder, private luv2ShopFormService: Luv2ShopFormService) { }
+  constructor(private formBuilder: FormBuilder, 
+    private luv2ShopFormService: Luv2ShopFormService, 
+    private cartService: CartService) { }
 
   ngOnInit(): void {
+
+    this.reviewCartDetails();
 
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
@@ -84,6 +89,7 @@ export class CheckoutComponent implements OnInit {
       }
     );
   }
+  
 
   get firstName(){return this.checkoutFormGroup.get('customer.firstName');}
   get lastName(){return this.checkoutFormGroup.get('customer.lastName');}
@@ -175,6 +181,12 @@ export class CheckoutComponent implements OnInit {
         formGroup!.get('state')!.setValue(data[0]);
       }
     );
+  }
+
+  reviewCartDetails() {
+    // subscribe to cartService.totalQuantity and cartService.totalPrice
+    this.cartService.totalQuantity.subscribe(data => this.totalQuantity = data);
+    this.cartService.totalPrice.subscribe(data => this.totalPrice = data);
   }
 
 }
